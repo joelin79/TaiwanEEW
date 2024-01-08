478ce4c56865ced83981f93559a77b7f06d2f952
Joe Lin <31405333+joelin79@users.noreply.github.com>
2024-01-08 19:49:51 +0800

    `AnalyticsManager` fix

--- TaiwanEEW/AnalyticsManager.swift
+++ TaiwanEEW/AnalyticsManager.swift
@@ -5,23 +5,23 @@
 //  Created by Joe Lin on 2023/9/7.
 //
 
-//import SwiftUI
-//import FirebaseAnalytics
-//import FirebaseAnalyticsSwift
-//
-//final class AnalysicsManager {
-//    static let shared = AnalysicsManager()
-//    private init() { }
-//    
-//    func logEvent(name: String, params: [String:Any]? = nil) {
-//        Analytics.logEvents(name, parameters: params)
-//    }
-//    
-//    func setUserId(userId: String) {
-//        Analytics.setUserID(userId)
-//    }
-//    
-//    func setUserProperty(value: String?, property: String?) {
-//        Analytics.setUserProperty(value, forName: property)
-//    }
-//}
+import SwiftUI
+import FirebaseAnalytics
+import FirebaseAnalyticsSwift
+
+final class AnalysicsManager {
+    static let shared = AnalysicsManager()
+    private init() { }
+    
+    func logEvent(name: String, params: [String:Any]? = nil) {
+        Analytics.logEvent(name, parameters: params)
+    }
+    
+    func setUserId(userId: String) {
+        Analytics.setUserID(userId)
+    }
+    
+    func setUserProperty(value: String?, property: String) {
+        Analytics.setUserProperty(value, forName: property)
+    }
+}
