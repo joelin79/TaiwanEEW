0531e18707b9234abe7836f35df80dee4aa0cc8f
Joe Lin <31405333+joelin79@users.noreply.github.com>
2024-02-28 13:13:02 +0800

    Minor changes (debug mode)

--- TaiwanEEW/AnalyticsManager.swift
+++ TaiwanEEW/AnalyticsManager.swift
@@ -11,17 +11,28 @@ import FirebaseAnalyticsSwift
 
 final class AnalysicsManager {
     static let shared = AnalysicsManager()
+    let debug = true
+    
     private init() { }
     
     func logEvent(name: String, params: [String:Any]? = nil) {
         Analytics.logEvent(name, parameters: params)
+        if(debug){
+            print("logEvent: name=\(name) param=\(String(describing: params))")
+        }
     }
     
     func setUserId(userId: String) {
         Analytics.setUserID(userId)
+        if(debug){
+            print("setUserId: id=\(userId)")
+        }
     }
     
     func setUserProperty(value: String?, property: String) {
         Analytics.setUserProperty(value, forName: property)
+        if(debug){
+            print("setUserProperty: value=\(String(describing: value)) property=\(property)")
+        }
     }
 }
